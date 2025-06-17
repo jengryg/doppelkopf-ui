14b11d5334781ebc576cbb19ee17d3273c7698a5
jengryg <62914315+jengryg@users.noreply.github.com>
2025-06-17 14:36:39 +0200

    Initialize lobby page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {Lobby} from './lobby/lobby';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: Lobby,
+    title: "Doppelkopf"
+  },
+  // {
+  //   path: "game/:id",
+  //   component: Game
+  // }
+];
--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -9,17 +9,4 @@ describe('App', () => {
       providers: [provideZonelessChangeDetection()]
     }).compileComponents();
   });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, doppelkopf-ui');
-  });
 });
