1bd5d684e893aa0013b5617dfe7175e535f5fe50
jengryg <62914315+jengryg@users.noreply.github.com>
2025-08-06 02:17:24 +0200

    Add environment setup for backend url.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,47 +2,46 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {catchError, map, Observable, of, tap} from 'rxjs';
 import {User} from './state/auth.models';
-
-const uri = 'http://localhost:16000/'
+import {environment} from '../../environments/environment';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
-    constructor(private http: HttpClient) {
-    }
+  constructor(private http: HttpClient) {
+  }
 
-    whoAmI(): Observable<User> {
-        return this.http.get<User>(`${uri}v1/auth/status`, {
-            withCredentials: true
-        }).pipe(
-            tap(response => console.log(response)),
-        )
-    }
+  whoAmI(): Observable<User> {
+    return this.http.get<User>(`${environment.backendUrl}/v1/auth/status`, {
+      withCredentials: true
+    }).pipe(
+      tap(response => console.log(response)),
+    )
+  }
 
-    login(username: string, password: string) {
-        const body = new HttpParams()
-            .set('username', username)
-            .set('password', password);
+  login(username: string, password: string) {
+    const body = new HttpParams()
+      .set('username', username)
+      .set('password', password);
 
-        return this.http.post(`${uri}v1/auth/login`, body, {
-            withCredentials: true,
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-            observe: 'response'
-        });
-    }
+    return this.http.post(`${environment.backendUrl}/v1/auth/login`, body, {
+      withCredentials: true,
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      observe: 'response'
+    });
+  }
 
-    register(username: string, password: string, passwordConfirm: string) {
-        return this.http.post<User>(`${uri}v1/users`, {
-            username, password, passwordConfirm
-        });
-    }
+  register(username: string, password: string, passwordConfirm: string) {
+    return this.http.post<User>(`${environment.backendUrl}/v1/users`, {
+      username, password, passwordConfirm
+    });
+  }
 
-    logout(): Observable<boolean> {
-        return this.http.get(`${uri}v1/auth/logout`, {
-            withCredentials: true
-        }).pipe(
-            tap(response => console.log(response)),
-            map(() => true),
-            catchError(() => of(false))
-        )
-    }
+  logout(): Observable<boolean> {
+    return this.http.get(`${environment.backendUrl}/v1/auth/logout`, {
+      withCredentials: true
+    }).pipe(
+      tap(response => console.log(response)),
+      map(() => true),
+      catchError(() => of(false))
+    )
+  }
 }
